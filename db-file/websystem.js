7ec85b494963aade62d5e415285fb17cc2b757dc
IS499 <kamw1420k@outlook.com>
2021-02-13 19:27:31 +0300

    db Connection

--- db-file/websystem.js
+++ db-file/websystem.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const mysql = require("mysql");
+
 
 const websystem = express();
+
+
+const db = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password:'',
+   database:'researcher-login'
+});
+
+db.connect((error)=> {
+   if (error){
+      console.log(error)
+   }
+   else{
+      console.log("MYSQL IS Connected")
+   }
+});
+
 
 websystem.get("/", (req, res) => {
    res.send("<h1> Home Page </h1>")
