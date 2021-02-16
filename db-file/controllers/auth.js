5649ccc8380598d9b369f3a622512e57b9a3636a
IS499 <kamw1420k@outlook.com>
2021-02-16 20:35:46 +0300

    update

--- db-file/controllers/auth.js
+++ db-file/controllers/auth.js
@@ -1,6 +1,36 @@
+const mysql = require("mysql");
+
+
+// we use process.env for security => detrnv .
+//we can creat file with coonection and importit whene we whant.
+const db = mysql.createConnection({
+   host: process.env.DATABASE_HOST,
+   user: process.env.DATABASE_USER,
+   password:process.env.DATABASE_PASSWORD,
+   database:process.env.DATABASE
+});
 
 
 exports.researcherSignup = (req, res) =>{
    console.log(req.body);
+
+   const {name, email, college, deptName, mobNum, country, level, university, password, passwordConfirm}= req.body;
+
+   db.query('SELECT email FROM studentresearcher WHERE email = ?',[email], (error, results) =>{
+      if(error){
+         console.log(error)
+      }
+
+      if(results.length > 0){
+         return res.render('researcherSignup',{
+            message:'The email is already in use'
+         })
+      }
+      else if (password !== passwordConfirm){
+         return res.render('researcherSignup',{
+            message:'password do not match'
+         })
+      }
+   })
    res.send("Form Submited");
 }
