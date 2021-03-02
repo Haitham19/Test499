3cdbbb786b4cd96898e3b8ec2db38682103b8d22
Haitham19 <438100684@student.ksu.edu.sa>
2021-03-02 20:05:00 +0300

    Update

--- db-file/routes/pages.js
+++ db-file/routes/pages.js
@@ -5,10 +5,16 @@ const router = express.Router();
 
 
 // res.render => this is to git the thing that you do it in the folder .
-//
+
+// Home page.
 router.get('/',(req,res) =>{
 res.render('homePage')
 });
+
+
+
+
+// Researcehr pages.
 router.get('/ResHomePage',(req,res) =>{
    res.render('ResHomePage')
 });
@@ -22,8 +28,19 @@ router.get('/researcherSignup',(req,res) =>{
    res.render('researcherSignup')
 });
 
+
+
+// Center General Manager pages.
 router.get('/CGMhomepage',(req,res) =>{
    res.render('CGMhomepage')
 });
+
+router.get('/CGMcomm',(req,res) =>{
+   res.render('CGMcomm')
+});
+
+
+
+
 
 module.exports = router;
