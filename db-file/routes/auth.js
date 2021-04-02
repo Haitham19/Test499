cf9b9e69f8c9ec0c506291449f793eb4318deddb
IS499 <kamw1420k@outlook.com>
2021-04-02 22:09:49 +0300

    update

--- db-file/routes/auth.js
+++ db-file/routes/auth.js
@@ -1,34 +1,32 @@
 const { request } = require("express");
 const express = require("express");
-const authController = require('../controllers/auth');
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-
 // res.render => this is to git the thing that you do it in the folder .
 //
-router.post('/researcherSignup', authController.researcherSignup);
-router.post('/OrgResSignup', authController.OrgResSignup);
-router.post('/MinistrySignup',authController.MinistrySignup);
-router.post('/deanSignup',authController.deanSignup);
-router.post('/deputySignup',authController.deputySignup);
-router.post('/advisorSignup',authController.advisorSignup);
-router.post('/missionSignup',authController.missionSignup);
-router.post('/CGMSignup',authController.CGMSignup);
-router.post('/RDSignup',authController.RDSignup);
-router.post('/userLogin',authController.userLogin);
-router.post('/SRaddnewrequest', authController.SRaddnewrequest);
-router.post('/orgANR', authController.orgANR);
-router.post('/SRIupdateinfo', authController.SRIupdateinfo);
-router.get('/logout',authController.logout);
-router.post('/Duser', authController.Duser);
-router.post('/Uuser', authController.Uuser);
-router.post('/advisorUP', authController.advisorUP);
-router.post('/deanUP', authController.deanUP);
-router.post('/deputyUP', authController.deputyUP);
-router.post('/ministryUP', authController.ministryUP);
-router.post('/rdUP', authController.rdUP);
-router.post('/cgmUP', authController.cgmUP);
-
+router.post("/researcherSignup", authController.researcherSignup);
+router.post("/OrgResSignup", authController.OrgResSignup);
+router.post("/MinistrySignup", authController.MinistrySignup);
+router.post("/deanSignup", authController.deanSignup);
+router.post("/deputySignup", authController.deputySignup);
+router.post("/advisorSignup", authController.advisorSignup);
+router.post("/missionSignup", authController.missionSignup);
+router.post("/CGMSignup", authController.CGMSignup);
+router.post("/RDSignup", authController.RDSignup);
+router.post("/userLogin", authController.userLogin);
+router.post("/SRaddnewrequest", authController.SRaddnewrequest);
+router.post("/orgANR", authController.orgANR);
+router.post("/SRIupdateinfo", authController.SRIupdateinfo);
+router.get("/logout", authController.logout);
+router.post("/Duser", authController.Duser);
+router.post("/Uuser", authController.Uuser);
+router.post("/advisorUP", authController.advisorUP);
+router.post("/deanUP", authController.deanUP);
+router.post("/deputyUP", authController.deputyUP);
+router.post("/ministryUP", authController.ministryUP);
+router.post("/rdUP", authController.rdUP);
+router.post("/cgmUP", authController.cgmUP);
 
 module.exports = router;
