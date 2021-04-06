1845b275257adfeed9d324b10e6a29fd1e17872f
Haitham19 <438100684@student.ksu.edu.sa>
2021-04-06 23:32:59 +0300

    Added genInbox + genCOMM

--- db-file/routes/pages.js
+++ db-file/routes/pages.js
@@ -459,6 +459,25 @@ router.get("/genU", authController.isLoggedIn, (req, res) => {
     res.redirect("/userLogin");
   }
 });
+router.get("/genCOMM", authController.isLoggedIn, (req, res) => {
+  if (req.user) {
+    res.render("genCOMM", {
+      user: req.user,
+    });
+  } else {
+    res.redirect("/userLogin");
+  }
+});
+router.get("/genInbox", authController.isLoggedIn, (req, res) => {
+  if (req.user) {
+    res.render("genInbox", {
+      user: req.user,
+    });
+  } else {
+    res.redirect("/userLogin");
+  }
+});
+
 
 
 
