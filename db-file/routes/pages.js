5777b2e7aea3a73e3c9717bb45214cb1d8b6734a
Haitham19 <438100684@student.ksu.edu.sa>
2021-04-06 20:09:08 +0300

    Added genCOMM router, hbs, and Updates

--- db-file/routes/pages.js
+++ db-file/routes/pages.js
@@ -393,6 +393,16 @@ router.get("/genU", authController.isLoggedIn, (req, res) => {
     res.redirect("/userLogin");
   }
 });
+router.get("/genCOMM", authController.isLoggedIn, (req, res) => {
+  if (req.user) {
+    res.render("genCOMM", {
+      user: req.user,
+    });
+  } else {
+    res.redirect("/userLogin");
+  }
+});
+
 
 //-----------------------Education-----------------//
 router.get("/eduHP", authController.isLoggedIn, (req, res) => {
